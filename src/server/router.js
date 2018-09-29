91f23c7e07591cfbab683038d66238667517a5b3
Jon Preece <jonpreece@Jons-MacBook-Pro.local>
2018-09-29 12:39:44 +0100

    Routing fixes

--- src/server/router.js
+++ src/server/router.js
@@ -1,15 +1,27 @@
 import { renderToString } from 'react-dom/server'
 import * as React from 'react'
-import { StaticRouter } from 'react-router-dom'
+import { StaticRouter, matchPath } from 'react-router-dom'
 
 import page from './page'
 import App from '../client/App'
 
+const routes = ['/', '/about']
+
 function router(req, res) {
+  const match = routes.reduce(
+    (acc, route) => matchPath(req.url, { path: route, exact: true }) || acc,
+    null
+  )
+
+  if (!match) {
+    res.status(404).send('page not found')
+    return
+  }
+
   const context = {}
 
   const html = renderToString(
-    <StaticRouter location={req.url} context={context}>
+    <StaticRouter context={context} location={req.url}>
       <App />
     </StaticRouter>
   )
